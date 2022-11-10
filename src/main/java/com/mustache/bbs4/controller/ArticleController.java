7b4fa98fed89bdeb2591bfc0bf66291b69535f9f
박은빈 <joypeb@naver.com>
2022-11-10 11:03:58 +0900

    first commit

---
+++ src/main/java/com/mustache/bbs4/controller/ArticleController.java
@@ -0,0 +1,19 @@
+package com.mustache.bbs4.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping("articles")
+public class ArticleController {
+
+    @GetMapping("")
+    public String index() {
+        return "";
+    }
+
+
+}
